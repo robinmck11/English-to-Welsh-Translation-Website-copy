bf982cc77fe8f86705d41fa576eded7bbf25eda4
robinmck11 <robin.mck11@hotmail.co.uk>
2017-03-18 19:40:28 +0000

    SimpleDataSource

---
+++ src/main/database/SimpleDataSource.java
@@ -0,0 +1,53 @@
+package com.mycompany.database;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * A simple data source for getting database connections.
+ */
+
+public class SimpleDataSource
+
+{
+    /**
+     * Initializes the data source.
+     *
+     * @param fileName the name of the property file that contains the database
+     * driver, url, username and password
+     */
+    
+    
+    public static void init(InputStream fileName) throws IOException, ClassNotFoundException {
+        
+        Properties props = new Properties();
+        props.load(fileName);
+
+        String driver = props.getProperty("jdbc.driver");
+        url = props.getProperty("jdbc.url");
+        username = props.getProperty("jdbc.username");
+        password = props.getProperty("jdbc.password");
+
+        Class.forName(driver);
+    }
+
+    /**
+     * Gets a connection to the database.
+     *
+     * @return the database connection
+     */
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url,
+                username, password);
+    }
+
+    private static String url;
+    private static String username;
+    private static String password;
+}
