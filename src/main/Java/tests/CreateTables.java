7c44c38ff08d7dd9a8a6247c35962e8a726e38e4
robinmck11 <robin.mck11@hotmail.co.uk>
2017-03-19 18:42:31 +0000

    CreateTables v.2 - Robin

--- src/main/Java/tests/CreateTables.java
+++ src/main/Java/tests/CreateTables.java
@@ -1,12 +1,47 @@
 package tests;
 
+import database.SimpleDataSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by robin on 19/03/2017.
  */
 public class CreateTables
 {
-    public CreateTables()
-    {
+    private InputStream inputStream;
+    private Connection conn;
 
+    public CreateTables() throws SQLException {
+        inputStream = HSQLTester.class.getResourceAsStream("/database.properties");
+
+        try
+        {
+            SimpleDataSource.init(inputStream);
+            conn = SimpleDataSource.getConnection();
+            createTables();
+
+        } catch (IOException | ClassNotFoundException | SQLException e)
+        {
+            e.printStackTrace();
+        } finally {
+            conn.close();
+        }
+    }
+
+    private void createTables() throws SQLException {
+        Statement st = conn.createStatement();
+
+        st.executeUpdate("CREATE TABLE student\n" +
+                "(\n" +
+                "username varchar(20) NOT NULL,\n" +
+                " salt CHAR(20),\n" +
+                " hash CHAR(128),\n" +
+                " PRIMARY KEY (username)\n" +
+                ");");
     }
 }
