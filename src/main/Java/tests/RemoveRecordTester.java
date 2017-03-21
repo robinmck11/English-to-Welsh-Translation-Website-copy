e99c7d831e0ae74231f4ba2efce8eba20a0f2974
robinmck11 <robin.mck11@hotmail.co.uk>
2017-03-21 20:38:08 +0000

    RemoveRecordTester v.1

---
+++ src/main/Java/tests/RemoveRecordTester.java
@@ -0,0 +1,17 @@
+package tests;
+
+import database.RemoveRecord;
+
+/**
+ * Created by robin on 21/03/2017.
+ */
+public class RemoveRecordTester
+{
+    public static void main(String[] args)
+    {
+        RemoveRecord rR = new RemoveRecord();
+
+        rR.removeRecord("student","username", "student2");
+        rR.closeConnection();
+    }
+}
